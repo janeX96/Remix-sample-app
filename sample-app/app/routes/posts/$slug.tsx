c6426118d7f83a7cd9623fcd2f41d9eaf4d0a146
Janek <janek.klejn@gmail.com>
2023-03-24 11:54:10 +0100

    feat(posts): add markdown parser

--- sample-app/app/routes/posts/$slug.tsx
+++ sample-app/app/routes/posts/$slug.tsx
@@ -20,7 +20,7 @@ export default function PostSlug() {
   const { post, html } = useLoaderData<typeof loader>();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>{" "}
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
